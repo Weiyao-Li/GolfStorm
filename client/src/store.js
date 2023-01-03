ae09e306b7e433a0f55a48a1024c2a57cbecb544
Weiyao-Li <96925606+Weiyao-Li@users.noreply.github.com>
2023-01-02 22:32:34 -0500

    add/remove cart, cart design

--- client/src/store.js
+++ client/src/store.js
@@ -1,13 +1,20 @@
 import {combineReducers, configureStore} from '@reduxjs/toolkit'
 import thunk from 'redux-thunk'
 import {productListReducer, productDetailsReducer} from "./reducers/productReducers";
+import {cartReducer} from "./reducers/cartReducers";
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    cart: cartReducer
 })
 
-const initialState = {}
+const cartItemsFromStorage = localStorage.getItem('cartItems') ?
+    JSON.parse(localStorage.getItem('cartItems')) : []
+
+const initialState = {
+    cart: {cartItems: cartItemsFromStorage}
+}
 
 const middleware = [thunk]
 
@@ -18,3 +25,4 @@ const store = configureStore({
 });
 
 export default store;
+
